f6c5148a6ce354265821b937aa0f2da9d4bc4b6d
Krishna Teja Pogadadanda <kpogadadanda@miraclesoft.com>
2019-01-18 08:15:09 -0600

    pushing webapp code

---
+++ azure.js
@@ -0,0 +1,13 @@
+const app = require('express')();
+const cors = require('cors');
+
+app.use(cors());
+
+app.get('/azure',(req,res)=>{
+    console.log('azure request');
+    res.send('this is azure api');
+})
+
+app.listen(4000,()=>{
+    console.log('azure app running locally');
+})
